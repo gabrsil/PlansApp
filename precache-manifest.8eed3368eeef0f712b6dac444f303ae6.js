02eed0fc6f5d67f3980a584b5e74edbacfa904ba
Gabriel Angelo <gabriel.angelsad1@gmail.com>
2020-06-29 17:39:38 -0300

    Updates

---
+++ precache-manifest.8eed3368eeef0f712b6dac444f303ae6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907dd748cf2c69a7a97fd8d47dc0898d",
+    "url": "/VizirTest/index.html"
+  },
+  {
+    "revision": "8a561f99e018d0b192c8",
+    "url": "/VizirTest/static/css/main.b169f129.chunk.css"
+  },
+  {
+    "revision": "9786fb106f95d1b16808",
+    "url": "/VizirTest/static/js/2.4bfb326b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VizirTest/static/js/2.4bfb326b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a561f99e018d0b192c8",
+    "url": "/VizirTest/static/js/main.7247427d.chunk.js"
+  },
+  {
+    "revision": "c8eb1dc9c066879e9e34",
+    "url": "/VizirTest/static/js/runtime-main.e6fdbfff.js"
+  },
+  {
+    "revision": "d8ae748018dbc8c5d851bf5ad63b5f3f",
+    "url": "/VizirTest/static/media/undraw_mobile_pay_9abj.d8ae7480.svg"
+  }
+]);
